83972214e82f838c0d426b83608c5378e01eeae2
arpit ruhela <ruhelaarpit63@gmail.com>
2025-11-17 23:15:36 +0530

    create eliment

--- script.js
+++ script.js
@@ -1,89 +1,32 @@
-var arr = [
-    {
-        team: 'CSK',
-        primary: 'yellow',
-        secondary: 'blue',
-        trophy: 5,
-        captain: "RUTURAJ",
-    },
-    {
-        team: 'RCB',
-        primary: 'red',
-        secondary: 'black',
-        trophy: 5,
-        captain: "RAJAT PATIDAR",
-    },
-    {
-        team: 'MI',
-        primary: 'blue',
-        secondary: 'gold',
-        trophy: 5,
-        captain: "HARDIK PANDYA",
-    },
-    {
-        team: 'KKR',
-        primary: 'purple',
-        secondary: 'gold',
-        trophy: 2,
-        captain: "AJINKYA RAHANE",
-    },
-    {
-        team: 'SRH',
-        primary: 'orange',
-        secondary: 'black',
-        trophy: 1,
-        captain: "PET COMINS",
-    },
-    {
-        team: 'PBKS',
-        primary: 'crimson',
-        secondary: 'gold',
-        trophy: 0,
-        captain: "SHERYAS IYER",
-    },
-    {
-        team: 'DC',
-        primary: 'blue',
-        secondary: 'red',
-        trophy: 0,
-        captain: "KL RAHUL",
-    },
-    {
-        team: 'LSG',
-        primary: 'skyblue',
-        secondary: 'red',
-        trophy: 0,
-        captain: "RISHAB PANT",
-    },
-    {
-        team: 'RR',
-        primary: 'pink',
-        secondary: 'darkblue',
-        trophy: 1,
-        captain: "SENJAU SEMSON",
-    },
-    {
-        team: 'GT',
-        primary: 'darkblue',
-        secondary: 'gold',
-        trophy: 1,
-        captain: "SHUBMAN GILL",
-    },
-]
-var h1 = document.querySelector("h1")
-var h2 = document.querySelector("h2")
-var h3 = document.querySelector("h3")
 var main = document.querySelector(".main")
 var button = document.querySelector("button")
 
 button.addEventListener("click", function(){
+    var arr = ["hello i am Arpit","i am BCA student","sheryians is best","i am developer","i am fromm uttar pradesh"]
 
-  var winner = arr [Math.floor( Math.random()*arr.length)]
+    var a = Math.floor( Math.random()*arr.length)
 
-  h1.innerHTML = winner.team
-  h2.innerHTML = winner.captain
-  h3.innerHTML = winner.trophy
+    var h1 = document.createElement('h1')
 
-  main.style.backgroundColor = winner.primary
-  h1.style.backgroundColor = winner.secondary
+    h1.innerHTML = arr[a]
+
+    h1.style.position = 'absolute'
+
+    var x = Math.floor(Math.random()*100)
+    var y = Math.floor(Math.random()*100)
+    var rot = Math.floor(Math.random()*360)
+    var scl =Math.floor(Math.random()*3)
+
+    var c1 = Math.floor(Math.random()*256)
+    var c2 = Math.floor(Math.random()*256)
+    var c3 = Math.floor(Math.random()*256)
+
+    h1.style.left = x+"%"
+    h1.style.top = y+"%"
+    h1.style.color = `rgb(${c1},${c2},${c3})`
+    h1.style.rotate = rot +"deg"
+    h1.style.scale = scl
+
+    main.appendChild(h1)
+
 })
