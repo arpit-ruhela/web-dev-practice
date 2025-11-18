3859a2617d396b713a99ee6694ec62f20d72abee
arpit ruhela <ruhelaarpit63@gmail.com>
2025-11-18 23:13:24 +0530

    downlode

--- script.js
+++ script.js
@@ -1,32 +1,23 @@
-var main = document.querySelector(".main")
-var button = document.querySelector("button")
+var grow = 0
+var h2 = document.querySelector("h2")
+var btn = document.querySelector("button")
+var inner = document.querySelector(".inner")
 
-button.addEventListener("click", function(){
-    var arr = ["hello i am Arpit","i am BCA student","sheryians is best","i am developer","i am fromm uttar pradesh"]
+btn.addEventListener('click', function (){
+    btn.style.pointerEvents = 'none'
+    var num = 50 + Math.floor(Math.random()*50)
+    console.log('Your file will be downloaded in',num/10,'seconds');
 
-    var a = Math.floor( Math.random()*arr.length)
 
-    var h1 = document.createElement('h1')
+   var ar = setInterval(() => {
+        grow++
+        h2.innerHTML = grow + '%'
+        inner.style.width = `${grow}%`
+    }, num);
 
-    h1.innerHTML = arr[a]
-
-    h1.style.position = 'absolute'
-
-    var x = Math.floor(Math.random()*80)
-    var y = Math.floor(Math.random()*80)
-    var rot = Math.floor(Math.random()*360)
-    var scl =Math.floor(Math.random()*3)
-
-    var c1 = Math.floor(Math.random()*256)
-    var c2 = Math.floor(Math.random()*256)
-    var c3 = Math.floor(Math.random()*256)
-
-    h1.style.left = x+"%"
-    h1.style.top = y+"%"
-    h1.style.color = `rgb(${c1},${c2},${c3})`
-    h1.style.rotate = rot +"deg"
-    h1.style.scale = scl
-
-    main.appendChild(h1)
-
+    setTimeout(() => {
+        clearInterval(ar)
+        btn.innerHTML = "Downloded"
+        btn.style.opacity = 0.5
+    }, num*100);
 })
